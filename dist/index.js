1d3eac8da3e7873b9115450618b4702dfb006a1a
Gustavo Plaza <gustavo.adolfo@live.cl>
2020-02-18 16:23:05 -0300

    Se borra todo

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var jsGantt = require("./src/jsgantt");
-module.exports = jsGantt.JSGantt;
-exports.JSGantt = jsGantt.JSGantt;
-//# sourceMappingURL=index.js.map
